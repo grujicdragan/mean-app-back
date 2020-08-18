05405cfa47bd3546791be148c91de02db1ba950c
grujicdragan <dragan_grujic93@hotmail.com>
2020-08-18 21:35:38 +0200

    aaa

--- app.js
+++ app.js
@@ -26,8 +26,16 @@ const port = process.env.PORT || 3000;
 
 app.use(cors({ "origin": "*" }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+app.use(function(req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:https://grujicdragan.github.io/mean-app-ng/');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     next();
 });
 
