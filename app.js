d9bd197d98bd35c36d835fb733658f2bd26338df
grujicdragan <dragan_grujic93@hotmail.com>
2020-08-18 20:09:37 +0200

    1st

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.database, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log("Connected")
+}).catch((err) => {
+    console.error('Database error: ' + err);
+    process.exit(-1);
+});
+
+const app = express();
+
+const users = require('./routes/users');
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb+srv://dragan123:loganprogamer@mean-app.zgsz0.mongodb.net/timeTracker?retryWrites=true&w=majority',
+    secret: 'andjela97'
+}
