26b510c14c1b6a467859b1ebf5d73d03b720621c
grujicdragan <dragan_grujic93@hotmail.com>
2020-08-19 11:16:52 +0200

    aaa

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/timeTracker',
+    database: 'mongodb+srv://dragan123:dragan123@cluster0.zrkkv.mongodb.net/timeTracker?retryWrites=true&w=majority',
     secret: 'andjela97'
 }
