e106159543f478fad6bb35896bdbc5e5e16490ce
grujicdragan <dragan_grujic93@hotmail.com>
2020-08-21 12:08:24 +0200

    email fixed

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,7 @@ router.post('/register', (req, res, next) => {
 
     User.addUser(newUser, (err, user) => {
         if (err) {
-            res.json({ success: false, msg: 'Failed to register user' });
+            res.json({ success: false, msg: 'This email already exists!' });
         } else {
             res.json({ success: true, msg: 'User registered!' });
         }
